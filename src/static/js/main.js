02b86c019efa95fe62224d280ab125e5d556cba0
vincentdu <vincentdu@tcl.com>
2018-02-25 18:13:53 +0800

    commit

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,7 +1,42 @@
-const class2type = {};
 
-"Boolean Number String Function Array Date RegExp Object Error".split(" ").forEach((item) => {
-  class2type[ "object " + item ] = item.toLowerCase();
-});
 
-console.log(class2type);
+var Zepto = (function () {
+  var zepto = {}, $
+
+  function Z(doms) {
+    var len = doms.length 
+    for (var i = 0; i < len; i++) {
+      this[i] = doms[i]
+    }
+    this.length = doms.length
+  }
+
+  zepto.Z = function(doms) {
+    return new Z(doms)
+  }
+
+  zepto.init = function(doms) {
+    var doms = ['domObj1','domObj2','domObj3']
+    return zepto.Z(doms)
+  }
+
+  $ = function() {
+    return zepto.init()
+  }
+
+  $.fn = {
+    constructor: zepto.Z,
+    method: function() {
+      return this
+    }
+  }
+
+  zepto.Z.prototype = Z.prototype = $.fn
+
+  return $
+})()
+
+window.Zepto = Zepto
+window.$ === undefined && (window.$ = Zepto)
+
+console.log($().method());
